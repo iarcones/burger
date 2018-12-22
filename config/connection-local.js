55077bdcb643ac68dec753c5da73433ad39fa6c3
Isabel <iarcones@gmail.com>
2018-12-22 14:01:58 -0800

    including mysql host heroku

--- config/connection-local.js
+++ config/connection-local.js
@@ -1,10 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -12,7 +8,6 @@ var connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
-};
 
 // Make connection.
 connection.connect(function(err) {
