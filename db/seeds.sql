2099edc977df603a9560a316abd02d0003b37a91
Isabel <iarcones@gmail.com>
2018-12-22 13:26:18 -0800

    version working, pending css and readme

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('WHOPPER');
+INSERT INTO burgers (burger_name) VALUES ('Bacon & Cheese WHOPPER');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon WHOPPER');
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Bacon Crispy Chicken');
